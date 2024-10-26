8868dba4b9001d9bdc26481fbc501ad3015da581
Xzoul <jimmyprima.delacruzjr@bicol-u.edu.ph>
2024-10-27 00:16:06 +0800

    Initial Commit

---
+++ functional-task/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'functional.task',
+  appName: 'functional-task',
+  webDir: 'www'
+};
+
+export default config;
